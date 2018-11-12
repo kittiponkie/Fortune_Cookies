8387a473bdc30d2b456f558ff44e5d249a421329
kittipon <kittipon.kei@gmail.com>
2018-11-12 23:57:52 +0700

    done page 3

--- app/src/main/java/com/egco428/a13343/fortunecookies/Setting.kt
+++ app/src/main/java/com/egco428/a13343/fortunecookies/Setting.kt
@@ -1,18 +1,22 @@
 package com.egco428.a13343.fortunecookies
 
+import android.app.Activity
+import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
 import com.egco428.a13343.fortunecookies.DataSource.CommentDataSource
 import com.egco428.a13343.fortunecookies.Model.Comment
 
 import kotlinx.android.synthetic.main.activity_setting.*
 import kotlinx.android.synthetic.main.content_setting.*
+import kotlinx.android.synthetic.main.sublist.view.*
 import java.util.*
 
 class Setting : AppCompatActivity() {
@@ -43,9 +47,8 @@ class Setting : AppCompatActivity() {
 
         dataSource = CommentDataSource(this)
         dataSource!!.open()
-        val values = dataSource!!.allComments
-
-        val adapter = ArrayAdapter<Comment>(this,android.R.layout.simple_list_item_1,values)
+        var values = dataSource!!.allComments
+        var adapter = CourseArrayAdapter(this, 0, values!!)
         _listMessage.adapter = adapter
         _listMessage.setOnItemClickListener { adapterView, view, position, id ->
             val item = adapterView.getItemAtPosition(position)
@@ -55,34 +58,44 @@ class Setting : AppCompatActivity() {
             if(_listMessage.adapter.getCount() > 0){
                 comment = _listMessage.adapter.getItem(position) as Comment
                 dataSource!!.deleteComment(comment!!)
-                adapter.remove(comment)
+                //adapter.remove(comment)
             }
+            values = dataSource!!.allComments
+            adapter = CourseArrayAdapter(this, 0, values!!)
+            _listMessage.adapter = adapter
             adapter.notifyDataSetChanged()
         }
     }
 
     fun addMessage(view: View){
         //aaaa.text = spinner.selectedItem.toString()
+        dataSource!!.createComment(messageText.text.toString(),spinner.selectedItem.toString())
+        val values = dataSource!!.allComments
+        val adapter = CourseArrayAdapter(this, 0, values!!)
+        _listMessage.adapter = adapter
+        adapter.notifyDataSetChanged()
     }
 
     fun OnClick(view: View){
-        val adapter = _listMessage.adapter as ArrayAdapter<Comment>
         var comment : Comment? = null
         when(view.getId()){
             R.id.t1->{
                 val comments = arrayOf("Summer","Winter","Spring","Fall","Raining")
                 val nextInt = Random().nextInt(5)
                 comment = dataSource!!.createComment(comments[nextInt],"1")
-                adapter.add(comment)
+                //adapter.add(comment)
             }
             R.id.t2->{
                 if(_listMessage.adapter.getCount() > 0){
                     comment = _listMessage.adapter.getItem(0) as Comment
                     dataSource!!.deleteComment(comment!!)
-                    adapter.remove(comment)
+                    //adapter.remove(comment)
                 }
             }
         }
+        val values = dataSource!!.allComments
+        val adapter = CourseArrayAdapter(this, 0, values!!)
+        _listMessage.adapter = adapter
         adapter.notifyDataSetChanged()
     }
 
@@ -94,5 +107,52 @@ class Setting : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         dataSource!!.close()
+    }
+
+
+
+
+
+
+    private class CourseArrayAdapter(var context: Context, resource: Int, var objects: List<Comment>): BaseAdapter(){
+        override fun getCount(): Int {
+            return objects.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return objects[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+            val course = objects[position]
+            val rowMain:View
+            if(convertView == null){
+                val inflator = context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                rowMain  = inflator.inflate(R.layout.sublist, null)
+                val viewHolder = ViewHolder(rowMain.titleText,rowMain.subText)
+                rowMain.tag = viewHolder
+            }
+            else{
+                rowMain = convertView
+            }
+
+            val viewHolder = rowMain.tag as ViewHolder
+            viewHolder.title.text = course.commentData
+            viewHolder.sub.text = course.commentType
+            if(course.commentType.toString() == "Positive") viewHolder.title.setTextColor(Color.BLUE)
+            else if(course.commentType.toString() == "Negative") viewHolder.title.setTextColor(Color.rgb(243, 156, 18))
+
+
+            return rowMain
+        }
+
+        private class ViewHolder(val title: TextView, val sub: TextView){
+
+        }
     }
 }
